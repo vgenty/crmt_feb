4a5066a9492cd8c5b35e9be4a18a86d4fa352458
Victor Genty <vic.genty@gmail.com>
2013-10-11 22:50:40 -0500

    organizing

---
+++ pins_to_display/view_me.py
@@ -0,0 +1,35 @@
+from ROOT import TH2D, TCanvas, TLatex
+from FixROOT import OneFix
+from viewer_methods import *
+import sys
+      
+def main():
+    fixer=OneFix()
+    c1     = TCanvas("c1","c1",1000,500)
+    module = TH2D("mod",";x #rightarrow;y #rightarrow",64,0,64,4,0,4)
+    
+    pins_to_pixels   =  pins("pins.csv")
+    pixels_to_fibers =  mapit()
+    pins_to_count    =  struck("event_list.txt",sys.argv[1])
+    rows             =  rowmaker()
+    
+    
+    module = fillhisto(module,
+                       pins_to_pixels,
+                       pixels_to_fibers,
+                       pins_to_count,
+                       rows)
+    c1.cd()
+    title=fixer.fix(module,"Module")
+    module.GetYaxis().SetNdivisions(4)
+    module.GetXaxis().SetNdivisions(8)
+    module.Draw("COLZ")
+    title.Draw("SAMES")
+    c1.Update()
+    c1.Modified()
+    sys.stdin.readline()
+   
+   
+if __name__ == '__main__' :
+    main()
+    
